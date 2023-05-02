4267325db99ad483ca3ec087745c189d2b5415c6
yaroslav <yaroslav.babii98@gmail.com>
2023-05-02 23:22:05 +0300

    add NavLink from Dialogs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
               <div className='app-wrapper-content'>
                   <Routes>
                       <Route path="/profile" element={<Profile/>}/>
-                      <Route path="/dialogs" element={<Dialogs/>}/>
+                      <Route path="/dialogs/*" element={<Dialogs/>}/>
                       {/*<Route path="/news" element={<News/>}/>*/}
                       {/*<Route path="/music" element={<Music/>}/>*/}
                       {/*<Route path="/setting" element={<Setting/>}/>*/}
