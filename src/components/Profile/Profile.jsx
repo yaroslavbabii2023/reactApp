1bf8a731ba6db62fe330f4592fccee381da51ce2
yaroslav <yaroslav.babii98@gmail.com>
2023-05-04 00:21:51 +0300

    props from Dialog

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img
-                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
-            </div>
-            <div>
-                ava + description
-            </div>
-            <div>
-                <MyPosts/>
-            </div>
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
+
     )
 }
 
