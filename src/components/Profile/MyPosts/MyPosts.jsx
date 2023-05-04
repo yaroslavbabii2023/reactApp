cae302fc22603df6b98da3d2b65eebf1a6146b4f
yaroslav <yaroslav.babii98@gmail.com>
2023-05-04 14:41:45 +0300

    add method map

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+
+    let posts = [
+        {id: 1, message: 'Hi, how are you?', likeCount: 12},
+        {id: 2, message: 'Its my first post', likeCount: 3},
+    ]
+
+    let postsElements = posts.map(p => <Post message={p.message} like={p.likeCount}/>)
     return (
         <div className={s.postsBlock}>
             <h2>My posts</h2>
@@ -14,7 +21,7 @@ const MyPosts = (props) => {
             </div>
 
             <div className={s.posts}>
-                <Post like='1'/>
+                {postsElements}
             </div>
         </div>
     )
