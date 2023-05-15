0e4d9638dd37d705c6660eea34cb34e4b1c7ce64
yaroslav <yaroslav.babii98@gmail.com>
2023-05-15 18:51:57 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPostText} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={store.getState()} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 
 }
 
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,54 +1,58 @@
-let rerenderEntireTree = () => {
-    console.log("stay changed")
-}
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likeCount: 12},
-            {id: 2, message: 'Its my first post', likeCount: 3},
-            {id: 2, message: 'Its my ', likeCount: 5},
-        ],
-        newPostText: 'it-kamasutra.com',
 
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likeCount: 12},
+                {id: 2, message: 'Its my first post', likeCount: 3},
+                {id: 2, message: 'Its my ', likeCount: 5},
+            ],
+            newPostText: 'it-kamasutra.com',
+
+        },
+        dialogsPage: {
+            message:[
+                {id: 1, message: 'Hello'},
+                {id: 1, message: 'How are you'},
+                {id: 1, message: 'Yolo'},
+                {id: 1, message: 'Yo'},
+                {id: 1, message: 'English'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Yaroslav'},
+                {id: 2, name: 'Oleksiy'},
+                {id: 3, name: 'Petro'},
+                {id: 4, name: 'Andriy'},
+                {id: 5, name: 'Olga'},
+            ],
+        },
     },
-    dialogsPage: {
-        message:[
-            {id: 1, message: 'Hello'},
-            {id: 1, message: 'How are you'},
-            {id: 1, message: 'Yolo'},
-            {id: 1, message: 'Yo'},
-            {id: 1, message: 'English'},
-        ],
-        dialogs: [
-            {id: 1, name: 'Yaroslav'},
-            {id: 2, name: 'Oleksiy'},
-            {id: 3, name: 'Petro'},
-            {id: 4, name: 'Andriy'},
-            {id: 5, name: 'Olga'},
-        ],
+    getState() {
+        return this._state
     },
-}
+    _callSubscriber() {
+        console.log("stay changed")
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0,
+        }
 
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0,
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-   state.profilePage.newPostText = newText;
-    rerenderEntireTree(state)
-}
+export default store;
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-export default state;
+window.store = store;
