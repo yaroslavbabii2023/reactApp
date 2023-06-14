3cf74e43bf9ff34e2f756f66b759f1b54c0bdf34
yaroslavbabii2023 <yaroslav.babii2023@gmail.com>
2023-06-14 09:05:51 +0300

    create componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,13 +3,12 @@ import userPhoto from "../../assets/images/images.png";
 import axios from "axios";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items)
-        })
+        componentDidMount() {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
 
-    }
     render () {
         return <div>
             {
