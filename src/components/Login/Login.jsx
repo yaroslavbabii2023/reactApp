4c9052274edf7e13c89ca7f3a1846686c602205c
yaroslavbabii2023 <yaroslav.babii2023@gmail.com>
2023-07-18 18:21:39 +0300

    created FormsControls.js and added redux-form(field validation)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import {Field, reduxForm} from "redux-form";
+import {Input} from "../common/FormsControls/FormsControls";
+import {required} from "../utils/validator/validators";
 
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+                <Field placeholder={"Login"}
+                       validate={[required]}
+                       name={"login"} component={Input}/>
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+                <Field placeholder={"Password"}
+                       validate={[required]}
+                       name={"password"} component={Input}/>
             </div>
             <div>
-                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+                <Field component={Input} name={"rememberMe"} type={"checkbox"}/> remember me
             </div>
             <div>
                 <button>Login</button>
