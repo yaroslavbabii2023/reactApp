5ce01be02028077c31c6812cdc00081df75b6fa5
yaroslavbabii2023 <yaroslav.babii2023@gmail.com>
2023-07-20 08:58:27 +0300

    added in the App.js componentDidMount and created app-reducer.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,7 @@ import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from 'redux-form'
+import appReducer from "./app-reducer";
 
 let reducer = combineReducers({
     profilePage: profileReducer,
@@ -14,6 +15,7 @@ let reducer = combineReducers({
     usersPage: usersReducer,
     auth: authReducer,
     form: formReducer,
+    app: appReducer,
 });
 
 
